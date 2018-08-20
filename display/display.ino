e4ceafe319fb25a7728fd3bf1f8eaa9030ad1aa6
Ian Flanagan <nobodyx647x@gmail.com>
2018-08-20 03:15:37 -0400

    //TODO: regularly commit to git

--- display/display.ino
+++ display/display.ino
@@ -1,19 +1,69 @@
+/**
+ * 
+ * 
+ * all of this shit is depracated or whaterver
+ * 
+ * i redid this in micropython because im lazy and python is easy
+ * 
+ * 
+ * 
+ * 
+**/
+
+
+
+
+
+
 #include <ESP8266WiFi.h>
 
-char* APName = "test";
+IPAddress local(192, 168, 1, 2);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println();
+void setup() {
+    Serial.begin(115200);
+    Serial.println("yeet");
+    WiFi.softAPdisconnect(true);
 
-  Serial.printf("Starting AP named %f", APName);
-  WiFi.softAP(APName);
+    WiFi.softAPConfig(local, gateway, subnet);
+    boolean result = WiFi.softAP("ESP", "Password");
 
+    if (result) {
+        Serial.println("ITS WORKING!");
+    } else {
+        Serial.println("lol nope");
+    }
+    Serial.println(WiFi.softAPIP());
 }
 
+void loop() {
+    if (Serial.available() > 0) {
+        Serial.readString();
+        Serial.println("gotcha");
+        Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
 
-void loop()
-{
+        WiFiClient client;
+        IPAddress server(192, 168, 1, 50);
+        client.connect(server, 80);
+        Serial.println("connected and waiting");
+        client.print("gimmu ur data boi");
+        Serial.println("sent request");
+        unsigned long timeout = millis();
+        while (client.available() == 0) {
+            if (millis() - timeout > 5000) {
+                Serial.println(">>> Client Timeout !");
+                client.stop();
+                return;
+            }
+        }
 
+        // Read all the lines of the reply from server and print them to Serial
+        while (client.available()) {
+            String line = client.readString();
+            Serial.print(line);
+        }
+        
+    }
+    delay(50);
 }
--- distance/distance.ino
+++ distance/distance.ino
@@ -1,15 +1,19 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "Barker";
-const char* pw = "BarkerWifi";
+const char* ssid = "ESP";
+const char* pw = "Password";
 
-const int trigger = D0;
+const int trigger = D2;
 const int echo = D1;
+
 
 WiFiServer server(80);
 
 const bool debugMode = true;  // whether or not to use serial to print debug
                               // info
+
+const bool sleepMode = 1; //1 for deep sleep, 0 for delay
+
 
 void debugLog(String message, bool newline = true) {
     if (!debugMode) {
@@ -31,45 +35,125 @@ void debugLog(String message, bool newline = true) {
 
 long getDist() {
     digitalWrite(trigger, LOW);
-    delayMicroseconds(5);
+    delayMicroseconds(30);
+    if (digitalRead(echo) == HIGH) {
+        debugLog("wtf???");
+        delayMicroseconds(100);
+        return getDist();
+    }
     digitalWrite(trigger, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigger, LOW);
 
     pinMode(echo, INPUT);
     long duration = pulseIn(echo, HIGH);
+    debugLog(String((duration / 2) / 29.1));
 
     return (duration / 2) / 29.1;  // Distance in cm
+}
+
+long avgDist(int times) {  // Returns a smoothed distance given a number of
+                           // readings to take
+    long result = 0;
+
+    for (int i = 0; i < times; i++) {
+        result += getDist();
+    }
+
+    return result / times;
+}
+
+void sleep(int duration){
+  if (sleepMode == 1){
+    ESP.deepSleep(duration*1000);
+  }else{
+    delay(duration);
+  }
 }
 
 void setup() {
     pinMode(trigger, OUTPUT);
     pinMode(echo, INPUT);
-    debugLog("Connecting to network: ", true);
+    
+    debugLog("Connecting to network: ");
+}
 
+void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        debugLog("oof, we're disconnected");
+        if (networkOnline()){
+            debugLog("Network is online");
+            connectToESP();
+        }else{
+            debugLog("Network not online");
+            sleep(500);
+            return;
+        }
+
+    }
+    handle();
+    delay(50);
+}
+
+void connectToESP() {
     WiFi.begin(ssid, pw);
 
-    while (WiFi.status() != WL_CONNECTED) {  // Waits until
+    while (WiFi.status() != WL_CONNECTED) {  // Waits until connected to network
         delay(50);
         debugLog(".", false);
     }
     debugLog("We did it!");
+
+    // Static ip
+    IPAddress ip(192, 168, 1, 50);
+    IPAddress gateway(192, 168, 1, 1);
+    IPAddress subnet(255, 255, 255, 0);
+    WiFi.config(ip, gateway, subnet);
+
     server.begin();
     debugLog(WiFi.localIP().toString());
 }
 
-void loop() {
+bool networkOnline() {
+    debugLog("Scanning for networks");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+
+    int n = WiFi.scanNetworks();  // Number of networks detected
+
+    for (int i = 0; i < n; i++) {
+        if (WiFi.SSID(i) == "ESP") {  // Sorta like an array I guess? Idk how to explain it.
+                      // Wifi.ssid(i) gets the ssid of the ith network detected
+            debugLog("Found!");
+            return true;
+        }
+    }
+    return false;
+}
+
+void handle() {
     WiFiClient client = server.available();
     if (client) {
-        debugLog("Incoming connection from "+client.localIP().toString());
+        
+        debugLog("Incoming connection");
 
+        int timeout = 3000;
         while (!client.available()) {
             delay(1);
+            timeout --;
+
+            if (timeout < 0){
+                debugLog("Timeout while while waiting for client");
+                return;
+            }
         }
 
-        long dist = getDist();
+        long dist = avgDist(5);
 
-        debugLog("Distance measured as "+String(dist));
+        debugLog("Avg distance measured as " + String(dist));
         client.print(String(dist));
     }
 }
